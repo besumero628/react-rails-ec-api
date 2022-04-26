f98354d0281234b8aaa60249eb2dea5b6df0c4ed
besumero628 <besumero628@gmail.com>
2022-04-26 23:04:33 +0900

    [add] series migrate

--- db/migrate/20220426140314_create_series.rb
+++ db/migrate/20220426140314_create_series.rb
@@ -1,6 +1,7 @@
 class CreateSeries < ActiveRecord::Migration[7.0]
   def change
     create_table :series do |t|
+      t.string :name
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_25_125952) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_26_140314) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "series", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
