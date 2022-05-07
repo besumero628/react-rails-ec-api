a998a9f516ded052293c277c12ca8f4c730cd43b
besumero628 <besumero628@gmail.com>
2022-05-07 23:25:39 +0900

    [Add] order info

--- db/migrate/20220507142246_create_orderinfos.rb
+++ db/migrate/20220507142246_create_orderinfos.rb
@@ -1,6 +1,10 @@
 class CreateOrderinfos < ActiveRecord::Migration[7.0]
   def change
     create_table :orderinfos do |t|
+      t.references :user
+      t.references :address
+      t.string :to_name
+      t.integer :price
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_06_052858) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_07_142246) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_06_052858) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orderinfos", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "address_id"
+    t.string "to_name"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["address_id"], name: "index_orderinfos_on_address_id"
+    t.index ["user_id"], name: "index_orderinfos_on_user_id"
   end
 
   create_table "orderproducts", force: :cascade do |t|
